5fa20dced8d0d6d9bc84855f36fd30ff0dd53773
Mikael Lognseth <mikael.lognseth@arribatec.com>
2021-04-19 11:08:52 +0200

    Added admin check, better logging and MFA check

--- TenantReporter.ps1
+++ TenantReporter.ps1
@@ -3,7 +3,7 @@
 
 See the readme.md file for comprehensive details on how this is strung together
 
-You'll need the ExchangeOnlineManagement, SharePoint, MSonline and AzureADPreview modules for this script to run, if they are not isntalled they will be automatically installed for you.
+You"ll need the ExchangeOnlineManagement, SharePoint, MSonline and AzureADPreview modules for this script to run, if they are not isntalled they will be automatically installed for you.
 
 .NOTES
     Name:           TenantReporter
@@ -13,10 +13,18 @@ You'll need the ExchangeOnlineManagement, SharePoint, MSonline and AzureADPrevie
     Purpose/Change: Initial development
 #>
 
+Write-Host "Checking for elevated permissions..."
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+    Break
+    }
+else {
+    Write-Host "Script is running as administrator - executing code..." -ForegroundColor DarkGreen
 
-$ErrorActionPreference = "SilentlyContinue"
+    #$ErrorActionPreference = "SilentlyContinue"
 
-Function Get-AzureMFAStatus {
+    Function Get-AzureMFAStatus {
 
     [CmdletBinding()]
     param(
@@ -43,91 +51,171 @@ Function Get-AzureMFAStatus {
             foreach ($User in $UserPrincipalName) {
                 try {
                     Get-MsolUser -UserPrincipalName $User -ErrorAction Stop | select DisplayName, UserPrincipalName, `
-                        @{Name = 'isAdmin'; Expression = {if ($SkipAdminCheck) {Write-Output "-"} else {if ($AdminUsers -match $_.UserPrincipalName) {Write-Output $true} else {Write-Output $false}}}}, `
-                        @{Name = 'MFAEnabled'; Expression={if ($_.StrongAuthenticationMethods) {Write-Output $true} else {Write-Output $false}}}
+                        @{Name = "isAdmin"; Expression = {if ($SkipAdminCheck) {Write-Output "-"} else {if ($AdminUsers -match $_.UserPrincipalName) {Write-Output $true} else {Write-Output $false}}}}, `
+                        @{Name = "MFAEnabled"; Expression={if ($_.StrongAuthenticationMethods) {Write-Output $true} else {Write-Output $false}}}
 
                 } catch {
                     $Object = [pscustomobject]@{
-                        DisplayName       = '_NotSynced'
+                        DisplayName       = "_NotSynced"
                         UserPrincipalName = $User
-                        isAdmin           = '-'
-                        MFAEnabled        = '-'
+                        isAdmin           = "-"
+                        MFAEnabled        = "-"
                     }
                     Write-Output $Object
                 }
             }
         } else {
             $AllUsers = Get-MsolUser -MaxResults $MaxResults | Where-Object {$_.IsLicensed -eq $isLicensed} | select DisplayName, UserPrincipalName, `
-                @{Name = 'isAdmin'; Expression = {if ($SkipAdminCheck) {Write-Output "-"} else {if ($AdminUsers -match $_.UserPrincipalName) {Write-Output $true} else {Write-Output $false}}}}, `
-                @{Name = 'MFAEnabled'; Expression={if ($_.StrongAuthenticationMethods) {Write-Output $true} else {Write-Output $false}}}
+                @{Name = "isAdmin"; Expression = {if ($SkipAdminCheck) {Write-Output "-"} else {if ($AdminUsers -match $_.UserPrincipalName) {Write-Output $true} else {Write-Output $false}}}}, `
+                @{Name = "MFAEnabled"; Expression={if ($_.StrongAuthenticationMethods) {Write-Output $true} else {Write-Output $false}}}
 
             Write-Output $AllUsers | Sort-Object isAdmin, MFAEnabled -Descending
         }
     }
     END {}
-}
-
-#Setter PSGallery til "trusted repo"
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
-#Liste over relevante moduler
-
-$Modules = @(
-    "ExchangeOnlineManagement"
-    "MSonline"
-    "AzureADPreview"
-    "Microsoft.Online.SharePoint.PowerShell"
-    "Microsoft.Exchange.Management.ExoPowershellModule"
-)
-
-# Check whether or not the modules are installed already - if no, install them.
-foreach ($Module in $Modules) {
-    if (!(Get-InstalledModule -Name $Module)) {
-        Write-Host("$Module is not installed") -ForegroundColor Yellow
-        Write-Host("Installing $Module") -ForegroundColor Green
-		Install-Module -Name $Module -Confirm:$false -Force
-		Import-Module -Name $Module
-	}
-	else {
-		Write-Host("$Module is already installed.") -ForegroundColor Green
-	}
-}
-
-
-$FolderPath = "C:\Innit\"
-$OrganizationName = Read-Host("Organization name (The bit before .onmicrosoft): ")
-$SPOurl = "https://" + $OrganizationName + "-admin.sharepoint.com"
-$LogPath = Join-Path $FolderPath + "$OrganizationName.txt"
-
-Start-Transcript -Path $LogPath -Force
-
-#Authentication Flow.
-
-$title = ""
-$msg     = 'Does your account require MFA to sign in?'
-$options = '&Yes', '&No'
-$default = 1  # 0=Yes, 1=No
-
-do {
-    $response = $Host.UI.PromptForChoice($title, $msg, $options, $default)
-    if ($response -eq 0) {
-        # prompt for sign in using MFA
-        Connect-AzureAD
-        Connect-MsolService
-        Connect-SPOService -Url $SPOurl
-        Connect-ExchangeOnline
     }
-    if ($response -eq 1) {
-        #Prompt for sign in using basic / traditional auth.
-        Write-Host "Please enter valid Admin credentials" -f Magenta
-        $Creds = Get-Credential
-        Connect-ExchangeOnline -Credential $Creds
-        Connect-MsolService -Credential $Creds
-        Connect-SPOService -Url $SPOurl -Credential $Creds
-        Connect-AzureAD -Credential $Creds
+
+    #Sets PSGallery to "trusted repo"
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+    #All modules you will need for this script
+
+    $Modules = @(
+        "ExchangeOnlineManagement"
+        "MSonline"
+        "AzureAD"
+        "Microsoft.Online.SharePoint.PowerShell"
+    )
+
+    #Check whether or not the modules are installed already - if no, install them.
+    foreach ($Module in $Modules) {
+        if (!(Get-InstalledModule -Name $Module)) {
+            Write-Host($Module + " is not installed") -ForegroundColor Yellow
+            Write-Host("Installing " + $Module) -ForegroundColor Green
+    		Install-Module -Name $Module -Confirm:$false -Force -AllowClobber
+    		Import-Module -Name $Module -AllowClobber
+    	}
+    	else {
+    		Write-Host($Module + " is already installed.") -ForegroundColor Green
+    	}
     }
-} until ($response -eq 1 -or $response -eq 0)
+
+    $CurrentTime = Get-Date -UFormat %T
+    $CurrentTime = $CurrentTime.ToString()
+    $FolderPath = "C:\Innit\"
+    $OrganizationName = Read-Host("Organization name (The bit before .onmicrosoft)")
+    $SPOurl = "https://" + $OrganizationName + "-admin.sharepoint.com"
+    $LogPath = $FolderPath + $OrganizationName + "_" + $CurrentTime + ".txt"
+
+    if (!($FolderPath)) {
+        New-Item -Path "C:\" -Name "Innit" -ItemType Directory
+    }
+
+    Start-Transcript -Path $LogPath -Force
+
+    #Authentication Flow.
+
+    $title = ""
+    $msg     = "Does your account require MFA to sign in?"
+    $options = "&Yes", "&No"
+    $default = 1  # 0=Yes, 1=No
+
+    do {
+        $response = $Host.UI.PromptForChoice($title, $msg, $options, $default)
+        if ($response -eq 0) {
+            #Prompt for sign in using MFA
+            Write-Host("Since you are using MFA; you will be prompted to sign in to each service individually") -f Magenta
+            Connect-AzureAD
+            Connect-MsolService
+            Connect-SPOService -Url $SPOurl
+            Connect-ExchangeOnline
+        }
+        if ($response -eq 1) {
+            #Prompt for sign in using basic / traditional auth.
+            Write-Host "Please enter valid Admin credentials" -f Magenta
+            $Creds = Get-Credential
+            Connect-ExchangeOnline -Credential $Creds
+            Connect-MsolService -Credential $Creds
+            Connect-SPOService -Url $SPOurl -Credential $Creds
+            Connect-AzureAD -Credential $Creds
+        }
+    } until ($response -eq 1 -or $response -eq 0)
+
+
+    $Users = Get-Mailbox -RecipientTypeDetails UserMailbox
+
+    $LicensedUsers = (Get-MsolUser | where {$_.IsLicensed -eq $true}).Count
+    Write-Host("Your organization currently has " + $LicensedUsers + " Licensed users") -ForegroundColor Cyan
+    $AuthPolicyUsers = 0
+    Write-Host("Getting authentication policies for all users") -ForegroundColor Cyan
+    foreach ($user in $users) {
+        $AuthenticationPolicy = Get-AuthenticationPolicy -Identity $user.emailaddress
+        if ($AuthenticationPolicy -ne $true) {
+            $AuthPolicyUsers++
+        }
+    }
+
+    Write-Host("$AuthPolicyUsers users do not have an authentication policy") -ForegroundColor DarkYellow
+    Write-Host("Getting OAuthStatus")
 
 
 
-Stop-Transcript
+    $OAuthStatus = Get-OrganizationConfig | Select-Object OAuth2ClientProfileEnabled
+
+    if ($OAuthStatus -match $false) {
+        Write-Host("Modern authentication is not enabled for organization") -ForegroundColor Red
+    }
+    if ($OAuthStatus -match $true) {
+        Write-Host("Modern authentication is enabled for organization") -ForegroundColor Green
+    }
+
+    #Gets the date 60 days ago based on current time
+    $Users = Get-mailbox -RecipientTypeDetails UserMailbox
+    $60DaysBack = (Get-Date).AddDays(-60)
+    $UserLastSignInCount = 0
+
+    #Iterates through all users checking if they signed in after the given date, this can be prone to error as 365 does not give us a good way of fetching this data, but it gives us an    indication.
+
+    $LastLogon = Get-Mailbox -RecipientTypeDetails UserMailbox -Resultsize Unlimited | Get-MailboxStatistics | Select-Object DisplayName,LastLogonTime
+
+    foreach ($User in $LastLogon) {
+        if ($LastLogon.LastLogonTime -ge $60DaysBack) {
+            $UserLastSignInCount++
+        }
+    }
+
+    Write-Host("Getting MFA Status of all users")
+
+    $MFAUsers = 0
+
+    foreach ($User in $Users) {
+        $UserMFAStatus = Get-AzureMFAStatus -UserPrincipalName $User.UserPrincipalName
+        if ($UserMFAStatus.MFAEnabled -eq $false) {
+            $MFAUsers++
+        }
+    }
+
+    if ($MFAUsers -ne 0) {
+        Write-Host("$MFAUsers user(s) have not enabled MFA") -ForegroundColor Red
+    } else {
+        Write-Host("All users have enabled MFA") -ForegroundColor Green
+    }
+
+    #Get all SharePoint sites and see if they are shared externally.
+
+    $Sites = Get-SPOSite
+
+    foreach ($Site in $Sites) {
+        $SiteSharing = $Site.SharingCapability.ToString()
+        $SiteTitle = $Site.Title
+        $SiteUrl = $Site.Url
+        if ($SiteSharing -eq "ExternalUserAndGuestSharing") {
+            Write-Host($SiteTitle + " is shared externally!`nSite URL: " + $SiteUrl)
+        }
+    }
+
+    Disconnect-AzureAD
+    Disconnect-ExchangeOnline
+
+    Stop-Transcript
+}
